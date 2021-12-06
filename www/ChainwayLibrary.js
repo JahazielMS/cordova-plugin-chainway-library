2216a74a694bd043b3fa570e9b11cc53037c9d44
Jahaziel Medina <jmedina@app-lab.mx>
2021-12-06 10:40:52 -0600

    Método Iresult

--- www/ChainwayLibrary.js
+++ www/ChainwayLibrary.js
@@ -1,30 +1,34 @@
 var exec = require('cordova/exec');
 
-// exports.coolMethod = function (arg0, success, error) {
-//     exec(success, error, 'MainActivity', 'coolMethod', [arg0]);
-// }
-
-var execute = require('cordova/exec');
-
-var chainwayScan = {
-  coolMethod: function() {
-    return execute(null, null, 'MainActivity', 'coolMethod', [])
-  },
-  start: function() {
-    return execute(null, null, 'MainActivity', 'startScan', [])
-  },
-  stop: function() {
-    return execute(null, null, 'MainActivity', 'stopScan', [])
-  },
-  open: function() {
-    return execute(null, null, 'MainActivity', 'open', [])
-  },
-  close: function() {
-    return execute(null, null, 'ChainwayScannerPlugin', 'close', [])
-  },
-  IResult: function() {
-    return execute(null, null, 'MainActivity', 'IResult', [])
-  }
+exports.coolMethod = function(arg0, success, error) {
+  exec(success, error, 'MainActivity', 'coolMethod', [arg0])
 }
 
-module.exports = chainwayScan
+exports.IResult = function(arg0, success, error) {
+  exec(success, error, 'MainActivity', 'IResult', [arg0])
+}
+
+// var execute = require('cordova/exec');
+
+// var chainwayScan = {
+//   coolMethod: function() {
+//     return execute(null, null, 'MainActivity', 'coolMethod', [])
+//   },
+//   start: function() {
+//     return execute(null, null, 'MainActivity', 'startScan', [])
+//   },
+//   stop: function() {
+//     return execute(null, null, 'MainActivity', 'stopScan', [])
+//   },
+//   open: function() {
+//     return execute(null, null, 'MainActivity', 'open', [])
+//   },
+//   close: function() {
+//     return execute(null, null, 'MainActivity', 'close', [])
+//   },
+//   IResult: function() {
+//     return execute(null, null, 'MainActivity', 'IResult', [])
+//   }
+// }
+
+// module.exports = chainwayScan
