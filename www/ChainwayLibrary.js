e5e71e948ec71575026c1654b6ff043b445d1130
Jahaziel Medina <jmedina@app-lab.mx>
2021-12-06 17:53:44 -0600

    Function IResult return cordova context

--- www/ChainwayLibrary.js
+++ www/ChainwayLibrary.js
@@ -1,23 +1,20 @@
 var exec = require('cordova/exec');
 
-
-
-var chainway = {
-  IResult: function() {
-    return execute(null, null, 'MainScan', 'IResult', [])
-  }
-}
+// var chainway = {
+//   IResult: function() {
+//     return execute(null, null, 'MainScan', 'IResult', [])
+//   }
+// }
 
 // exports.coolMethod = function(arg0, success, error) {
 //   exec(success, error, 'MainScan', 'coolMethod', [arg0])
 // }
 
-// exports.IResult = function(arg0, success, error) {
-//   exec(success, error, 'MainScan', 'IResult', [])
-// }
+exports.IResult = function(args, success, error) {
+  exec(success, error, 'MainScan', 'IResult', [])
+}
 
 // var execute = require('cordova/exec');
-
 // var chainwayScan = {
 //   coolMethod: function() {
 //     return execute(null, null, 'MainActivity', 'coolMethod', [])
