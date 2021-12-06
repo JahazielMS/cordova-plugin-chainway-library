9c5891c5347cf6e167e5bfe5c10eed2c727168e1
Jahaziel Medina <jmedina@app-lab.mx>
2021-12-06 09:48:37 -0600

    Se agregaron métodos en plugin

--- www/ChainwayLibrary.js
+++ www/ChainwayLibrary.js
@@ -1,5 +1,30 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'ChainwayLibrary', 'coolMethod', [arg0]);
-};
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'MainActivity', 'coolMethod', [arg0]);
+// }
+
+var execute = require('cordova/exec');
+
+var chainwayScan = {
+  coolMethod: function() {
+    return execute(null, null, 'MainActivity', 'coolMethod', [])
+  },
+  start: function() {
+    return execute(null, null, 'MainActivity', 'startScan', [])
+  },
+  stop: function() {
+    return execute(null, null, 'MainActivity', 'stopScan', [])
+  },
+  open: function() {
+    return execute(null, null, 'MainActivity', 'open', [])
+  },
+  close: function() {
+    return execute(null, null, 'ChainwayScannerPlugin', 'close', [])
+  },
+  IResult: function() {
+    return execute(null, null, 'MainActivity', 'IResult', [])
+  }
+}
+
+module.exports = chainwayScan
