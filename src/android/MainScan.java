394aacdd781e25069cc15ec4e9df9df2754b8949
Jahaziel Medina <jmedina@app-lab.mx>
2021-12-07 10:20:48 -0600

    Create new ScanAcivity to get Scan Context

--- src/android/MainScan.java
+++ src/android/MainScan.java
@@ -8,7 +8,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainScan extends CordovaActivity {
+public class MainScan extends CordovaActivity implements ScanActivity {
 
     // @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
