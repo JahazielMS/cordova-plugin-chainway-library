1374335fe5b17a73eb807a825d2a8ae6b21758b9
Jahaziel Medina <jmedina@app-lab.mx>
2022-10-12 15:57:49 -0500

    Chainway Scanner

--- src/android/MainScan.java
+++ src/android/MainScan.java
@@ -3,18 +3,19 @@ package com.al.chainwaylibrary;
 import android.content.Context;
 import android.content.Intent;
 
-import org.apache.cordova.*;
+import com.infra.entregas.MainActivity;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainScan extends CordovaPlugin {
-
-    public String Barcode = "";
+    MainActivity mainActivity = new MainActivity();
     
     // @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -25,6 +26,7 @@ public class MainScan extends CordovaPlugin {
         } else if (action.equals("IResult")) {
             String message = args.getString(0);
             this.ScanActivity(message, callbackContext);
+            mainActivity.Barcode = null;
             return true;
         }
         return false;
@@ -40,7 +42,7 @@ public class MainScan extends CordovaPlugin {
 
     private void ScanActivity(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message + " Barcode: " + Barcode);
+            callbackContext.success(mainActivity.Barcode);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
--- www/ChainwayLibrary.js
+++ www/ChainwayLibrary.js
@@ -13,27 +13,3 @@ exports.coolMethod = function(arg0, success, error) {
 exports.IResult = function(arg0, success, error) {
   exec(success, error, 'MainScan', 'IResult', [arg0])
 }
-
-// var execute = require('cordova/exec');
-// var chainwayScan = {
-//   coolMethod: function() {
-//     return execute(null, null, 'MainActivity', 'coolMethod', [])
-//   },
-//   start: function() {
-//     return execute(null, null, 'MainActivity', 'startScan', [])
-//   },
-//   stop: function() {
-//     return execute(null, null, 'MainActivity', 'stopScan', [])
-//   },
-//   open: function() {
-//     return execute(null, null, 'MainActivity', 'open', [])
-//   },
-//   close: function() {
-//     return execute(null, null, 'MainActivity', 'close', [])
-//   },
-//   IResult: function() {
-//     return execute(null, null, 'MainActivity', 'IResult', [])
-//   }
-// }
-
-// module.exports = chainwayScan
